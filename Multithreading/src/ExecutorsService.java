39689a65077383d1dd4f031921b2b0a2b9b5aacd
Deepak Kumbhar <Deepakk@emids.com>
2022-07-19 15:35:35 +0530

    [deepak]added:collection api and completable future interface

--- Multithreading/src/ExecutorsService.java
+++ Multithreading/src/ExecutorsService.java
@@ -5,38 +5,38 @@
  * if the number of tasks is more than the threads available.
  */
 
-/*Instantiation*/
-
+/*Instantiating ExecutorService*/
 /**
- * Instantiating ExecutorService
  * We can use Java ExecutorService to create a single thread, a pool of threads, or a scheduled pool of threads.
  * To assign a task to ExecutorService, we can use the following methods-
+ *
+ *  Creates a ExecutorService object having a single thread.
+ * ExecutorService executorService1 = Executors.newSingleThreadExecutor();
+ *
+ *  Creates a ExecutorService object having a pool of 10 threads.
+ * ExecutorService executorService2 = Executors.newFixedThreadPool(10);
+ *
+ *  Creates a scheduled thread pool executor with 10 threads. In scheduled thread //pool, we can schedule tasks of the threads.
+ * ExecutorService executorService3 = Executors.newScheduledThreadPool(10);
  */
-// Creates a ExecutorService object having a single thread.
-//ExecutorService executorService1 = Executors.newSingleThreadExecutor();
-
-// Creates a ExecutorService object having a pool of 10 threads.
-//ExecutorService executorService2 = Executors.newFixedThreadPool(10);
-
-// Creates a scheduled thread pool executor with 10 threads. In scheduled thread //pool, we can schedule tasks of the threads.
-//ExecutorService executorService3 = Executors.newScheduledThreadPool(10);
 
 
-/**To assign a task to ExecutorService, we can use the following methods-
- */
+
+/**To assign a task to ExecutorService, we can use the following methods-*/
 //execute(Runnable task)
 //submit(Runnable task) / submit(Callable<T> task)
 //invokeAny(Collection<? extends Callable<T>> tasks)
 //invokeAll(Collection<? extends Callable<T>> tasks)
 
-
-import java.util.concurrent.*;
 /**
  * The process of shutting down can be done by the following three methods-
  */
 //shutdown() method
 //shutdownNow() method
 //awaitTermination() method
+
+
+import java.util.concurrent.*;
 
 public class ExecutorsService {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
